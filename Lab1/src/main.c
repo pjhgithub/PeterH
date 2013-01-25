2d2bf791bbd12612f06eab8166c72df92963041e
Peter Hettwer <peter.j.hettwer.2@my.ndsu.edu>
2013-01-25 14:47:03 -0600

    Update to Lab 1 Final

--- Lab1/src/main.c
+++ Lab1/src/main.c
@@ -20,80 +20,64 @@
 
 #define Button1 (1<<2)
 
+//global variables
+
+int enable = 1;
+volatile unsigned long m;
+
+//interrupt functions
+
+
+void EINT3_IRQHandler(void)
+{
+	NVIC_DisableIRQ(EINT3_IRQn);
+	enable = !enable;
+	for(m = 0;m<900000;m++)
+	NVIC_EnableIRQ(EINT3_IRQn);
+	LPC_GPIOINT -> IO0IntClr |= Button1;
+}
+
 //main function
+
 
 int main(void)
 {
-	LPC_GPIO -> FIODIR |= LED0;
-	LPC_GPIO -> FIODIR |= LED1;
-	LPC_GPIO -> FIODIR |= LED2;
-	LPC_GPIO -> FIODIR |= LED3;
-	LPC_GPIO -> FIODIR |= LED4;
-	LPC_GPIO -> FIODIR |= LED5;
-	LPC_GPIO -> FIODIR |= LED6;
-	LPC_GPIO -> FIODIR |= LED7;
+	LPC_GPIO1 -> FIODIR |= LED0;
+	LPC_GPIO1 -> FIODIR |= LED1;
+	LPC_GPIO1 -> FIODIR |= LED2;
+	LPC_GPIO1 -> FIODIR |= LED3;
+	LPC_GPIO1 -> FIODIR |= LED4;
+	LPC_GPIO1 -> FIODIR |= LED5;
+	LPC_GPIO1 -> FIODIR |= LED6;
+	LPC_GPIO1 -> FIODIR |= LED7;
 
-	int LED[] = {LED0, LED1, LED2, LED3, LED4, LED5, LED6, LED7};
-	int k = 0;
-	int enable = 1;
-	
+	LPC_GPIOINT ->IO0IntEnR |= Button1;
+	NVIC_EnableIRQ(EINT3_IRQn);
+
+
+	int LED[8] = {LED0, LED1, LED2, LED3, LED4, LED5, LED6, LED7};
+	int i;
+	LPC_GPIO1 -> FIOPIN |= LED0 + LED1 + LED2 + LED3 + LED4 + LED5 + LED6 + LED7;
 	while(1)
 	{
-		if(k==0)
-		{
 			for(i = 0;i<=7;i++)
 			{
-				if(Button1)
+				if(enable)
 				{
-					enable = !enable;
+				LPC_GPIO1 -> FIOPIN |= LED[i-1] + LED[i+1];
+				LPC_GPIO1 -> FIOPIN &= ~LED[i];
+				for(m=0;m<800000;m++);
 				}
-				else if(i==0)
+			}
+			for(i = 7;i>=0;i--)
+			{
+				if(enable)
 				{
-					LPC_GPIO -> FIOPIN |= LED[i];
+				LPC_GPIO1 -> FIOPIN |= LED[i-1] + LED[i+1];
+				LPC_GPIO1 -> FIOPIN &= ~LED[i];
+				for(m=0;m<800000;m++);
 				}
-				else if(i>0 & i<7)
-				{
-					LPC_GPIO -> FIOPIN |= !LED[(i-1)];
-					LPC_GPIO -> FIOPIN |= LED[i];
-				}
-				else
-				{
-					LPC_GPIO -> FIOPIN |= !LED[(i-1)];
-					LPC_GPIO -> FIOPIN |= LED[i];
-					k = 1;
-				}
-				pause(100);
 			}
 		}
-
-		else
-		{
-			for(i = 7;i>=0;i--)
-				{
-					if (Button1)
-					{
-						Button1 = !Button1;
-					}
-					else if(i==7)
-					{
-						LPC_GPIO -> FIOPIN |= LED[i];
-					}
-					else if(i>0 & i<7)
-					{
-						LPC_GPIO -> FIOPIN |= !LED[(i+1)];
-						LPC_GPIO -> FIOPIN |= LED[i];
-					}
-					else
-					{
-						LPC_GPIO -> FIOPIN |= !LED[(i+1)];
-						LPC_GPIO -> FIOPIN |= LED[i];
-						k = 0;
-					}
-				}
-			pause(100);
-		}
-
-	}
-
 return 1;
 }
